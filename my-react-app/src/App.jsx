4112266c59167b9a64c89f8b0d2faf91cd56e2bd
Zizou10 <paolo.pilati93@gmail.com>
2026-03-17 18:04:20 +0100

    Milestone 1: Creazione BudgetContext con stato budgetMode booleano e setter + wrap dell'App.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BudgetContext } from "./context/BudgetContext";
 import DefaultLayout from "./layouts/DefaultLayout";
 import Home from "./pages/Home";
 import ChiSiamo from "./pages/ChiSiamo";
@@ -7,15 +8,17 @@ import Articolo from "./pages/Articolo";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element= {<DefaultLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/chi-siamo" element={<ChiSiamo />} />
-          <Route path="/prodotti" element={<Prodotti />} />
-          <Route path="/prodotti/:id" element={<Articolo />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <BudgetContext.Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route element= {<DefaultLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/chi-siamo" element={<ChiSiamo />} />
+            <Route path="/prodotti" element={<Prodotti />} />
+            <Route path="/prodotti/:id" element={<Articolo />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </BudgetContext.Provider>
   )
 }
---
+++ my-react-app/src/context/BudgetContext.jsx
@@ -0,0 +1,15 @@
+import { createContext, useState } from "react";
+
+const BudgetContext = createContext();
+
+function BudgetProvider ({children}) {
+    const [budgetMode, setBudgetMode] = useState(false);
+    return (
+        <BudgetContext.Provider value={{ budgetMode, setBudgetMode }}>
+            {children}
+        </BudgetContext.Provider>
+        )
+    }
+}
+
+export { BudgetContext }
